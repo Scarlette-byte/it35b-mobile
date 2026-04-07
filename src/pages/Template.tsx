b8f8fc53d6970a28a9814c08dc67cff0882cae9a
Scarlette-byte <mancawanscarlette36@gmail.com>
2026-04-07 15:37:57 +0800

    added template

--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -1,16 +1,25 @@
-//import { IonToolBar } from "@ionic/react"
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from
+//import { IonToolbar } from "@ionic/react"
+
+import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from "@ionic/react";
+
 
 const Template:React.FC = () => {
     return(
         <IonPage>
             <IonHeader>
                 <IonToolbar>
-                    <IonButtons slot= "start">
-                        <IonMenuButton></IonMenuButton>
-                    </IonButtons>
+                      <IonButtons slot= "start">
+                           <IonMenuButton></IonMenuButton>                          
+                      </IonButtons>
+                      <IonTitle>Template</IonTitle>
                 </IonToolbar>
             </IonHeader>
+            <IonContent fullscreen>
+                <h1>Template</h1>
+            </IonContent>
         </IonPage>
+           
     );
-}
+};
+
+export default Template;
